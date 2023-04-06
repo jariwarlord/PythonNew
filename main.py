c51b0054869e920b1f32fd0e3bad815980df01c1
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2023-04-06 20:32:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-from art import logo
+import turtle as t
 import random
 
-print(logo)
-level = input("Choose a difficulty. Type 'easy' or 'hard': \n ")
-number = random.randint(1, 100)
-guess = int(input("Guess a number between 1 and 100 \n"))
+tim = t.Turtle()
 
-if(level == "easy"):
-  turns = 10
-elif(level == "hard"):
-  turns = 5
-  
-while number != guess:
-    if turns == 0:
-        print(f"You have {turns} turns left")
-        break
-    else:
-        turns = turns - 1
-        if guess > number:
-            print(f"Too High you have {turns} turns left")
-            guess = int(input("Guess a number between 1 and 100 \n"))
+t.colormode(255)
 
-        elif number > guess:
-            print(f"Too Low youh have {turns} turns left")
-            guess = int(input("Guess a number between 1 and 100 \n"))
 
-else:
-    print(f"You win ! Answer was: {number}!")
+tim.speed("fastest")
 
-#Number Guessing Game Objectives:
+def draw_spi(size_of_gap):
+    for _ in range(int(360/ size_of_gap)):
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player.
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+draw_spi(5)
+
+
+
+screen = t.Screen()
+screen.exitonclick()
