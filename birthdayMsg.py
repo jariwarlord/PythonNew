cf294ab12be928e5e688b620fd83bb309ca4eabd
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2023-04-12 02:19:05 +0300

    Add files via upload

---
+++ birthdayMsg.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "qweqweqw@gmail.com"
+MY_PASSWORD = "qweqweqweq"
+data = pandas.read_csv("birthday.csv")
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+birthdays_dict = {
+    (birthday_month, birthday_day) : data_row
+}
+
+
+
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows() }
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_person["name"])
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=birthday_person["email"],
+                                msg=f"Subject: Happy Birthday!\n\n{contents}")
